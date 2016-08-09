a2039a3ba52b96dad97517d89d46582c5ab301b1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-08-09 02:31:27 +0200

    [tx-robot] updated from transifex

---
+++ l10n/fa.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "اطلاعیه ها",
+    "Delete" : "حذف",
+    "Announcing…" : "اعلام كردن…",
+    "Announced!" : "اعلام شد!",
+    "Announcement does not exist anymore" : "اطلاعیه دیگر وجود ندارد",
+    "The subject is too long or empty" : "موضوع زیادی بلند است یا خالی است",
+    "No Announcements" : "بدون اطلاعیه",
+    "There are currently no announcements…" : "در حال حاضر هیچ اطلاعیه ای وجود ندارد",
+    "Add announcement" : "افزودن اطلاعیه",
+    "Subject…" : "موضوع…",
+    "Your announcement…" : "اطلاعیه شما...",
+    "Announce" : "اعلام"
+},
+"nplurals=1; plural=0;");
