b38938d31263e1f194b4af1af91ee8e6025073e3
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-23 02:06:19 +0000

    [tx-robot] updated from transifex

---
+++ l10n/es_AR.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Anuncios",
+    "Delete" : "Borrar",
+    "Announcing…" : "Anunciando...",
+    "Announced!" : "Anunciado!",
+    "Announcement does not exist anymore" : "El anuncio no existe más",
+    "You announced %s" : "Anunciaste %s",
+    "%s announced %s" : "%s anunció %s",
+    "The subject is too long or empty" : "El asunto es demasiado grande o está vacío",
+    "%1$s announced “%2$s”" : "%1$s anunció \"%2$s\"",
+    "No Announcements" : "Sin Anuncios",
+    "There are currently no announcements…" : "Actualmente no hay anuncios...",
+    "Add announcement" : "Agregar anuncio",
+    "Subject…" : "Asunto...",
+    "Your announcement…" : "Su Anuncio...",
+    "Announce" : "Anunciar"
+},
+"nplurals=2; plural=(n != 1);");
