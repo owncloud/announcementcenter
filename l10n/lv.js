62c5fb2ad7efc937a9031b5ea87dec55c04508af
ownCloud Jenkins <jenkins@owncloud.com>
2017-09-15 02:06:19 +0000

    [tx-robot] updated from transifex

---
+++ l10n/lv.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Paziņojumi",
+    "Delete" : "Dzēst",
+    "Announcing…" : "Paziņošana...",
+    "Announced!" : "Paziņots!",
+    "Announcement does not exist anymore" : "Paziņojums vairs neeksistē",
+    "You announced %s" : "Tu paziņoji %s",
+    "%s announced %s" : "%s paziņoja %s",
+    "The subject is too long or empty" : "Temats ir vai nu pārāk garš, vai tukšs",
+    "%1$s announced “%2$s”" : "%1$spaziņoja \"%2$s\"",
+    "No Announcements" : "Nav paziņojumu",
+    "There are currently no announcements…" : "Šobrīd nav paziņojumu...",
+    "Add announcement" : "Pievienot paziņojumu",
+    "Subject…" : "Temats...",
+    "Your announcement…" : "Tavs paziņojums...",
+    "Announce" : "Paziņot"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);");
