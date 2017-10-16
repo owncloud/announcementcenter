c34a467262c488e9a73d737a5b5d5055917e9967
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-16 02:06:21 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Aankondigings",
+    "Delete" : "Skrap",
+    "Announcing…" : "Kondig tans aan…",
+    "Announced!" : "Aangekondig!",
+    "Announcement does not exist anymore" : "Aankondiging bestaan nie meer nie",
+    "You announced %s" : "U het %s aangekondig",
+    "%s announced %s" : "%s het %s aangekondig",
+    "The subject is too long or empty" : "Die onderwerp is òf te lank òf leeg",
+    "%1$s announced “%2$s”" : "%1$s het “%2$s” aangekondig",
+    "No Announcements" : "Geen Aankondigings",
+    "There are currently no announcements…" : "Daar is tans geen aankondigings nie…",
+    "Add announcement" : "Voeg aankondiging toe",
+    "Subject…" : "Onderwerp…",
+    "Your announcement…" : "U aankondiging…",
+    "Announce" : "Kondig Aan"
+},
+"nplurals=2; plural=(n != 1);");
