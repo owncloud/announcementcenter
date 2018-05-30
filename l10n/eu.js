62c8df25ebfd81086b6c34c2863cb97866500755
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-30 02:06:24 +0000

    [tx-robot] updated from transifex

---
+++ l10n/eu.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Iragarpenak",
+    "Delete" : "Ezabatu",
+    "Announcing…" : "Iragartzen...",
+    "Announced!" : "Iragarrita!",
+    "Announcement does not exist anymore" : "Iragarpena dagoeneko ez da existitzen",
+    "You announced %s" : "%siragarri duzu",
+    "%s announced %s" : "%sk %s iragarri du",
+    "The subject is too long or empty" : "Gaia oso luzea da edo hutsik dago",
+    "%1$s announced “%2$s”" : "%1$sk \"%2$s\" iragarri du",
+    "No Announcements" : "Iragarpenik ez",
+    "There are currently no announcements…" : "Une honetan ez dago iragarpenik....",
+    "Add announcement" : "Iragarpena gehitu",
+    "Subject…" : "Gaia...",
+    "Your announcement…" : "Zure iragarpena...",
+    "Announce" : "Iragarri"
+},
+"nplurals=2; plural=(n != 1);");
