e40bc5010fe1a632e50718e00e85c330ad476d36
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-31 00:12:50 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Ankündigungen",
+    "Delete" : "Löschen",
+    "Announcing…" : "Ankündigung wird gesendet…",
+    "Announced!" : "Angekündigt!",
+    "Announcement does not exist anymore" : "Die Ankündigung existiert nicht mehr",
+    "You announced %s" : "Du hast %s angekündigt",
+    "%s announced %s" : "%s hat %s angekündigt",
+    "The subject is too long or empty" : "Das Betreff ist zu lang oder leer",
+    "%1$s announced “%2$s”" : "%1$s hat “%2$s” angekündigt",
+    "No Announcements" : "Keine Ankündigungen",
+    "There are currently no announcements…" : "Es gibt derzeit keine Ankündigungen…",
+    "Add announcement" : "Ankündigungen machen",
+    "Subject…" : "Betreff…",
+    "Your announcement…" : "Deine Ankündigungen…",
+    "Announce" : "Ankündigen"
+},
+"nplurals=2; plural=(n != 1);");
