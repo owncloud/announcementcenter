b53ae415f8d48cdf3800e0aa39961f3d29b11abc
ownClouders <devops@owncloud.com>
2023-05-02 00:17:11 +0000

    [tx] updated from transifex

---
+++ l10n/ka_GE.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "განცხადებები",
+    "Delete" : "წაშლა",
+    "Announcing…" : "გამოცხადება…",
+    "Announced!" : "გამოცხადდა!",
+    "Announcement does not exist anymore" : "განცხადება აღარ არსებობს",
+    "You announced %s" : "თქვენ გამოაცხადეთ %s",
+    "%s announced %s" : "%s-მა გამოაცხადა %s",
+    "The subject is too long or empty" : "საგანი ძალიან გრძელი ან ცარიელია",
+    "%1$s announced “%2$s”" : "%1$s-მა გამოაცხადა “%2$s”",
+    "No Announcements" : "განცხადებების გარეშე",
+    "There are currently no announcements…" : "ამჟამად განცხადებები არ არსებობს…",
+    "Add announcement" : "განცხადების დამატება",
+    "Subject…" : "საგანი…",
+    "Your announcement…" : "თქვენი განცხადება…",
+    "Announce" : "გამოცხადება"
+},
+"nplurals=2; plural=(n!=1);");
