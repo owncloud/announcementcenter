f5002875441359d9a7e87b99513633b03a6313b3
ownClouders <devops@owncloud.com>
2024-02-14 07:56:02 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -8,7 +8,7 @@ OC.L10N.register(
     "Announcement does not exist anymore" : "Lajmërimi s’ekziston më",
     "You announced %s" : "Lajmëruat %s",
     "%s announced %s" : "%s lajmëroi %s",
-    "The subject is too long or empty" : "Subjekti është shumë i gjatë ose i zbrazët",
+    "The subject is too long or empty" : "Subjekti është shumë i gjatë, ose i zbrazët",
     "%1$s announced “%2$s”" : "%1$s lajmëroi “%2$s”",
     "No Announcements" : "S’ka Lajmërime",
     "There are currently no announcements…" : "Tani s’ka lajmërime…",
