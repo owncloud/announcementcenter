d099fb9b615c5618142a56d3d7e1fa6d8ec046fa
ownClouders <devops@owncloud.com>
2024-04-20 00:44:30 +0000

    [tx] updated from transifex

---
+++ l10n/ru_RU.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "announcementcenter",
+    {
+    "Announcements" : "Уведомления",
+    "Delete" : "Удалить",
+    "Announcing…" : "Уведомление...",
+    "Announced!" : "Уведомление отправлено!",
+    "Announcement does not exist anymore" : "Уведомления больше не существует",
+    "You announced %s" : "Вы уведомили %s",
+    "%s announced %s" : "%s уведомил %s",
+    "The subject is too long or empty" : "Тема слишком короткая или отстутствует",
+    "%1$s announced “%2$s”" : "%1$s уведомил “%2$s”",
+    "No Announcements" : "Нет уведомлений",
+    "There are currently no announcements…" : "На данный момент уведомления отсутствуют...",
+    "Add announcement" : "Добавить уведомление",
+    "Subject…" : "Тема...",
+    "Your announcement…" : "Ваше уведомление...",
+    "Announce" : "Отправить уведомление"
+},
+"nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
